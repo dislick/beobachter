3dd09b2fe9a857c09f8d9364077443d245fa9100
Patrick Muff <muff.patrick@muffag.ch>
2019-11-29 16:53:07 +0100

    refactor(core): remove @ts-ignore

--- packages/core/src/scrape_website.ts
+++ packages/core/src/scrape_website.ts
@@ -3,11 +3,14 @@ import puppeteer from 'puppeteer';
 export async function scrapeWebsite<T>(
   browser: puppeteer.Browser,
   url: string,
-  fn: string
+  fnString: string
 ): Promise<T> {
   const page = await browser.newPage();
+
   await page.goto(url);
-  // @ts-ignore
-  const value = await page.evaluate(Function(fn));
+
+  const fn = Function(fnString) as () => T;
+  const value = await page.evaluate(fn);
+
   return value;
 }
