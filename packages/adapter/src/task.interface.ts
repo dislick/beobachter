83570070835881377d64e1b6b7c0c6df5aec6f89
Patrick Muff <muff.pa@gmail.com>
2019-12-13 17:18:47 +0100

    feat: add type of tasks support

--- packages/adapter/src/task.interface.ts
+++ packages/adapter/src/task.interface.ts
@@ -1,7 +1,11 @@
 export interface Task {
+  type: 'browser' | 'http-json' | 'http-text';
   name: string;
   description: string;
   url: string;
-  fn: string;
   interval: number;
+
+  fn?: string; // browser
+  path?: string; // http-json
+  regex?: string; // http-text
 }
--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -3,6 +3,8 @@ import puppeteer from 'puppeteer';
 import { scrapeWebsite } from './scrape_website';
 import { AdapterConsole } from '@wetcher/adapter-console';
 import { Adapter, Task } from '@wetcher/adapter';
+import request from 'request-promise-native';
+import { get } from 'lodash';
 
 const config: {
   adapters: any;
@@ -14,11 +16,47 @@ const adapters: Adapter[] = config.adapters.map((entry: any) => {
 });
 
 async function bootstrap() {
+  const browserTasks = config.tasks.filter(task => task.type === 'browser');
+  const httpJsonTasks = config.tasks.filter(task => task.type === 'http-json');
+
+  runBrowserTasks(browserTasks);
+  runHttpJsonTasks(httpJsonTasks);
+}
+
+async function runBrowserTasks(tasks: Task[]) {
   const browser = await puppeteer.launch();
 
-  for (const task of config.tasks) {
+  for (const task of tasks) {
     setInterval(async () => {
+      if (!task.fn) {
+        throw new Error('You must provide property "fn" for a browser task');
+      }
+
       const value = await scrapeWebsite(browser, task.url, task.fn);
+
+      for (const adapter of adapters) {
+        adapter.record(task, value);
+      }
+    }, task.interval * 1000);
+  }
+}
+
+async function runHttpJsonTasks(tasks: Task[]) {
+  for (const task of tasks) {
+    setInterval(async () => {
+      if (!task.path) {
+        throw new Error(
+          'You must provide property "path" for a http-json task'
+        );
+      }
+
+      const response = await request.get(task.url, {
+        headers: {
+          'User-Agent': 'wetcher',
+        },
+        json: true,
+      });
+      const value = get(response, task.path);
 
       for (const adapter of adapters) {
         adapter.record(task, value);
